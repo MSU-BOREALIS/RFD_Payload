ddc293cc3e9511332d65e10bea4dbeae3899e019
Trevor <tgahl7@gmail.com>
2017-06-14 14:33:31 -0600

    removed temperature and xbee systems

--- Still_Images_and_Command_Relay_usbRadio.py
+++ Still_Images_and_Command_Relay_usbRadio.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 """
-Still Images and Command Relay for Raspberry Pi with Xbee and RFD 900+
+Still Images and Command Relay for Raspberry Pi with RFD 900+
 
 Author:	Austin Langford, AEM, MnSGC
 Based on work from the Montana Space Grant Consortium
 Software created for use by the Minnesota Space Grant Consortium
-Purpose: To communicate with a ground transceiver to receive commands, and relay them through the xbee                                                    #
-Additional Features: GPS beacon and Image Transmission
+Purpose: To transfer GPS and still image information
 Creation Date: March 2016
 """
 
@@ -27,7 +26,7 @@ import serial.tools.list_ports
 
 class GPSThread(threading.Thread):
     """ A thread to read in raw GPS information, and organize it for the main thread """
-    def __init__(self, threadID, gps, Q, exceptions, resetFlag,loggingGPS):			# Constructor
+    def __init__(self, threadID, gps, Q, exceptions, resetFlag, loggingGPS):			# Constructor
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.gpsSer = gps
@@ -38,7 +37,6 @@ class GPSThread(threading.Thread):
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
 
-        temp_sensor = '/sys/bus/w1/devices/28-00000522ec61/w1_slave'
         rfdPort = serial.Serial(port = "/dev/ttyAMA0", baudrate = 38400, timeout = 5)
         try:
             gpsPort = serial.Serial(port = '/dev/gps', baudrate = 9600, timeout = 3)
@@ -97,94 +95,6 @@ class GPSThread(threading.Thread):
         except Exception, e:
             self.exceptionsQ.put(str(e))
             self.resetFlagQ.put('gpsThread dead')
-
-class TempThread(threading.Thread):
-    """ A thread to read in raw temperature sensor information, and organize it for the main thread """
-    def __init__(self, threadID, Q, exceptions, resetFlag, loggingTemp):			# Constructor
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.tempQ = Q
-        self.exceptionsQ = exceptions
-        self.resetFlagQ = resetFlag
-        self.loggingTemp = loggingTemp
-
-    def temp_raw(self):
-        f = open(temp_sensor,'r')
-        lines = f.readlines()
-        f.close()
-        return lines
-
-    def read_temp(self):
-        lines = self.temp_raw()
-        temp_output = lines[1].find('t=')
-        if temp_output != 1:
-            temp_string = lines[1].strip()[temp_output+2:]
-            temp_c = float(temp_string)/1000.0
-            temp_f = temp_c*9.0/5.0 + 32.0
-            return temp_c, temp_f
-
-    def run(self):
-        global folder
-        try:
-            while True:					# Run forever
-                temp = self.read_temp()
-                self.tempQ.put(temp)
-
-                if self.loggingTemp:
-                    try:
-                        curTime = str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
-                        f = open(folder+"templog.txt","a")
-                        f.write(curTime+" - "+str(temp)+"\n")
-                        f.close()
-                    except Exception, e:
-                        print(str(e))
-                        self.exceptionsQ.put(str(e))
-
-        ### Catches unexpected errors ###
-        except Exception, e:
-            self.exceptionsQ.put(str(e))
-            self.resetFlagQ.put('tempThread dead')
-
-class XbeeReceiveThread(threading.Thread):
-    """ A thread to read information from the xbee, and send it to the main thread """
-    def __init__(self,threadID, xbee, xbeeReceived, exceptions, resetFlag):         # Constructor
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.xbee = xbee
-        self.receivedQ = xbeeReceived
-        self.exceptionsQ = exceptions
-        self.resetFlagQ = resetFlag
-
-    def run(self):
-        while self.xbee.isOpen():
-            try:
-                line = self.xbee.readline()         # Read a line from the xbee
-                self.receivedQ.put(line)        # Put the information in the receivedQ
-            except Exception, e:                    # Catch any unexpected error, and notify the main thread of them
-                self.exceptionsQ.put(str(e))
-                self.resetFlagQ.put('reset')
-
-
-class XbeeSendThread(threading.Thread):
-    """ A Thread to send information out through the xbee radio """
-    def __init__(self,threadID, xbee, xbeeToSend,exceptions,resetFlag):         # Constructor
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.xbee = xbee
-        self.sendQ = xbeeToSend
-        self.exceptionsQ = exceptions
-        self.resetFlagQ = resetFlag
-
-    def run(self):
-        while self.xbee.isOpen():
-            try:
-                while(not self.sendQ.empty()):              # If there are items in the sendQ, send them out the xbee
-                    temp = self.sendQ.get()
-                    self.xbee.write(str(self.sendQ.get()).encode())
-
-            except Exception, e:                            # Catch any unexpected error, and notify the main thread of them
-                self.exceptionsQ.put(str(e))
-                self.resetFlagQ.put('reset')
 
 class TakePicture(threading.Thread):
     """ Thread to take two pictures, one at full resolution, the other at the selected resolution """
@@ -319,7 +229,7 @@ class main:
 
     """ The main program class """
     def __init__(self):
-        global folder, loggingGPS, loggingTemp
+        global folder, loggingGPS
         self.folder = folder
 
         # Get a list of the usb devices connected and assign them properly
@@ -327,11 +237,7 @@ class main:
         serialConverters = []
         for each in ports:
             print(each.vid,each.pid,each.hwid,each.device)
-            if each.vid == 1027 and each.pid == 24597:
-                self.xPort = each.device
-                print('Xbee on ' + str(each.device))
-
-            elif each.vid == 1659 and each.pid == 8963:
+            if each.vid == 1659 and each.pid == 8963:
                 serialConverters.append(each)
 
         # The USB-serial converters don't have unique IDs, so they need to be checked
@@ -361,10 +267,6 @@ class main:
         self.gpsBaud = 9600
         self.gpsTimeout = 3
 
-        # XBee Serial Variables
-        self.xBaud = 9600
-        self.xTimeout = 3
-
         # Create the imagedata.txt file
         fh = open(self.folder + "imagedata.txt","w")
         fh.write("")
@@ -380,18 +282,9 @@ class main:
         self.takingPicture = False
 
         ### Create queues to share info with the threads
-        self.xSendQ = Queue.Queue()
-        self.xReceivedQ = Queue.Queue()
-        self.xReceivedExceptionsQ = Queue.Queue()
-        self.xSendExceptionsQ = Queue.Queue()
-        self.xReceivedResetQ = Queue.Queue()
-        self.xSendResetQ = Queue.Queue()
         self.gpsQ = Queue.LifoQueue()
         self.gpsExceptionsQ = Queue.Queue()
         self.gpsResetQ = Queue.Queue()
-        self.tempQ = Queue.LifoQueue()
-        self.tempExceptionsQ = Queue.Queue()
-        self.tempResetQ = Queue.Queue()
         self.picQ = Queue.Queue()
 
         ### Try to create the various serial objects; if they fail to be created, set them as disabled ###
@@ -414,16 +307,6 @@ class main:
                 self.ser.write('Alert: Camera is disabled\n')
             print('Camera disabled')
 
-        # Xbee
-        try:
-            self.xbee = serial.Serial(port = self.xPort, baudrate = self.xBaud, timeout = self.xTimeout)
-            self.xbeeEnabled = True
-        except:
-            self.xbeeEnabled = False
-            if(self.rfdEnabled):
-                self.ser.write('Alert: Xbee is disabled\n')
-            print('Xbee disabled')
-
         # GPS
         try:
             self.gps = gpsPort = serial.Serial(port = '/dev/gps', baudrate = 9600, timeout = 3)
@@ -434,38 +317,10 @@ class main:
                 self.ser.write('Alert: GPS is disabled\n')
             print('GPS disabled')
 
-        # Temp Sensor
-        try:
-            f = open(temp_sensor,'r')
-            lines = f.readlines()
-            f.close()
-            if(lines[0].strip()[-3:] != 'YES'):
-                self.tempEnabled = False
-                if(self.rfdEnabled):
-                    self.ser.write('Alert: Temp Sensor is disabled\n')
-                print('Temp Sensor Disabled')
-            else:
-                self.tempEnabled = True
-
-        except Exception, e:
-            print(str(e))
-            self.tempEnabled = False
-            if(self.rfdEnabled):
-                self.ser.write('Alert: Temp Sensor is disabled\n')
-            print('Temp Sensor Disabled')
-
         ### Start the appropriate side threads ###
-        # Xbee
-        if(self.xbeeEnabled):
-            self.startXbeeThreads()
-
         # GPS
         if(self.gpsEnabled):
             self.startGPSThread()
-
-        # Temp Sensor
-        if(self.tempEnabled):
-            self.startTempThread()
 
         # Get Started
         self.starttime = time.time()
@@ -475,9 +330,6 @@ class main:
 
     def getGPSCom(self):
         return [self.gpsPort,self.gpsBaud,self.gpsTimeout]
-
-    def getXbeeCom(self):
-        return [self.xPort,self.xBaud,self.xTimeout]
 
     def getRFDCom(self):
         return [self.rfdPort,self.rfdBaud,self.rfdTimeout]
@@ -528,6 +380,7 @@ class main:
 
     def send_image(self,exportpath):
         """ Sends the image through the RFD in increments of size self.wordlength """
+        sys.stdout.flush()
         timecheck = time.time()
         done = False
         cur = 0
@@ -565,6 +418,8 @@ class main:
 
     def mostRecentImage(self):
         """ Command 1: Send most recent image """
+        sys.stdout.flush()
+        self.ser.flush()
         self.ser.write('A')      # Send the acknowledge
         try:
             print "Send Image Command Received"
@@ -748,63 +603,19 @@ class main:
     def sendDeviceStatus(self):
         """ Returns the status of the serial devices to the ground station """
         try:
-            status = 'Camera: '+str(self.cameraEnabled)+', GPS: '+str(self.gpsEnabled)+', Xbee: '+str(self.xbeeEnabled) +', Temp: '+self.recentTemp + '\n'
+            status = 'Camera: '+str(self.cameraEnabled)+', GPS: '+str(self.gpsEnabled)+'
             self.ser.write(status)
             print('Status Sent')
         except Exception, e:
             print(str(e))
-
-
-    def startXbeeThreads(self):
-        # Xbee Receive Thread
-        self.xReceiveThread = XbeeReceiveThread("xbeeReceivedThread",self.xbee, self.xReceivedQ, self.xReceivedExceptionsQ, self.xReceivedResetQ)
-        self.xReceiveThread.daemon = True
-        self.xReceiveThread.start()
-
-        # Xbee Send Thread
-        self.xSendThread = XbeeSendThread("xbeeSendThread", self.xbee, self.xSendQ, self.xSendExceptionsQ, self.xSendResetQ)
-        self.xSendThread.daemon = True
-        self.xSendThread.start()
 
     def startGPSThread(self):
         self.gpsThread = GPSThread("gpsThread",self.gps, self.gpsQ, self.gpsExceptionsQ, self.gpsResetQ, loggingGPS)
         self.gpsThread.daemon = True
         self.gpsThread.start()
 
-    def startTempThread(self):
-        self.tempThread = TempThread("tempThread", self.tempQ, self.tempExceptionsQ, self.tempResetQ, loggingTemp)
-        self.tempThread.daemon = True
-        self.tempThread.start()
-
     def checkSideThreads(self):
         """ Check to make sure the side threads are still running """
-        # If either of the xbee threads need to be reset, reset them both, and recreate the xbee object just to make sure that the xbee wasn't the issue
-        if(self.xbeeEnabled):
-            if((not self.xReceivedResetQ.empty()) or (not self.xSendResetQ.empty())):
-                try:
-                    self.xbee.close()       # Try to close the xbee
-                    print('xbee closed')
-                except:
-                    pass
-
-                try:
-                    self.xbee = serial.Serial(port = self.xPort, baudrate = self.xBaud, timeout = self.xTimeout)        # Reopen the xbee
-                    self.xbee.close()
-                    self.xbee.open()
-                    print(self.xbee.isOpen())
-                    self.startXbeeThreads()     # Restart the threads
-
-                    # Empty the reset Qs
-                    while(not self.xReceivedResetQ.empty()):
-                        self.xReceivedResetQ.get()
-                    while(not self.xSendResetQ.empty()):
-                        self.xSendResetQ.get()
-                except Exception, e:
-                    print(str(e))
-                    self.xbeeEnabled = False        # If this fails, disable the xbee
-                    if(self.rfdEnabled):
-                        self.ser.write("Alert: Xbee is now disabled")
-                    print("Xbee is now Disabled")
 
         # If the gps thread needs to be reset, do it
         if(self.gpsEnabled):
@@ -826,33 +637,6 @@ class main:
                     if(self.rfdEnabled):
                         self.ser.write('Alert: GPS is disabled')
                     print("GPS is now Disabled")
-
-        if(self.tempEnabled):
-            if(not self.tempResetQ.empty()):
-                try:
-                    f = open(temp_sensor,'r')
-                    lines = f.readlines()
-                    f.close()
-                    if(lines[0].strip()[-3:] != 'YES'):
-                        self.tempEnabled = False     # If this fails, disable the gps
-                        if(self.rfdEnabled):
-                            self.ser.write('Alert: Temp Sensor is disabled')
-                        print("Temp Sensor is Disabled")
-                    else:
-                        self.tempEnabled = True
-                        self.tempResetQ.put('reset')
-                        if(self.rfdEnabled):
-                            self.ser.write('Temp Sensor is now Enabled')
-                        print("Temp Sensor Enabled")
-                        self.startTempThread()       # Restart the thread
-                        # Clear the temp reset Q
-                        while(not self.tempResetQ.empty()):
-                            self.tempResetQ.get()
-                except:
-                    self.tempEnabled = False     # If this fails, disable the gps
-                    if(self.rfdEnabled):
-                        self.ser.write('Alert: Temp Sensor is disabled')
-                    print("Temp Sensor is Disabled")
 
     def loop(self):
         """ The main loop for the program """
@@ -913,11 +697,6 @@ class main:
                 except:
                     self.rfdEnabled = False     # The only thing not exception handled in the command functions is the acknowledge send, so if this block is triggered, the RFD write failed
 
-                # If it's not a command for the raspberry pi, send it out the xbee
-                else:
-                    if(self.xbeeEnabled):
-                        if(command != ''):
-                            self.xSendQ.put(command)      # Adds the command to the xbee send list so the xbee thread can send it out
 
                 ### Send information to the ground station ###
                 # Send a GPS update through the RFD
@@ -927,22 +706,6 @@ class main:
                         self.ser.write(gps)
                         while(not self.gpsQ.empty()):
                             self.gpsQ.get()
-
-                # Temperature
-                if(self.tempEnabled):
-                    if(not self.tempQ.empty()):
-                        self.recentTemp = str(self.tempQ.get())
-                        print("Temperature: "+self.recentTemp)
-                        while(not self.tempQ.empty()):
-                            gpsExceptionsQ
-                            self.tempQ.get()
-                else:
-                    self.recentTemp = "False"
-
-                # Send out everything the xbee received
-                if(self.xbeeEnabled):
-                    while(not self.xReceivedQ.empty()):
-                        self.ser.write(self.xReceivedQ.get())
 
             self.checkSideThreads()             # Make sure the side threads are still going strong
 
@@ -991,16 +754,6 @@ class main:
             if(self.gpsEnabled):
                 while(not self.gpsExceptionsQ.empty()):
                     print(self.gpsExceptionsQ.get())
-            if(self.xbeeEnabled):
-                while(not self.xReceivedExceptionsQ.empty()):
-                    print(self.xReceivedExceptionsQ.get())
-                while(not self.xSendExceptionsQ.empty()):
-                    print(self.xSendExceptionsQ.get())
-            if(self.tempEnabled):
-                while(not self.tempExceptionsQ.empty()):
-                    print(self.tempExceptionsQ.get())
-
-            ### Check if there is now an Xbee, GPS, RFD, or Camera attached; if there is, get it going
 
             # Camera Check
             if(not self.cameraEnabled):
@@ -1013,43 +766,6 @@ class main:
                         self.ser.write('Camera is now Enabled')
                 except:
                     pass
-
-            # Temp Sensor Check
-            if(not self.tempEnabled):
-                try:
-                    f = open(temp_sensor,'r')
-                    lines = f.readlines()
-                    f.close()
-                    if(lines[0].strip()[-3:] != 'YES'):
-                        pass
-                    else:
-                        self.tempEnabled = True
-                        self.tempResetQ.put('reset')
-                        if(self.rfdEnabled):
-                            self.ser.write('Temp Sensor is now Enabled')
-                        print("Temp Sensor Enabled")
-
-                except Exception, e:
-                    pass
-
-            # Xbee Check
-            if (not self.xbeeEnabled):
-                ports = serial.tools.list_ports.comports()
-                for each in ports:
-                    if each.vid == 1027 and each.pid == 24597:      # The vid and pid for the xbee, start it up if they match one of the comports
-                        if(not self.xbeeEnabled):
-                            self.xPort = each.device
-                            try:
-                                self.xbee = serial.Serial(port = self.xPort, baudrate = self.xBaud, timeout = self.xTimeout)
-                                self.xbeeEnabled = True
-                                self.xSendResetQ.put('reset')
-                                self.xReceivedResetQ.put('reset')        # This will cause the threads to be restarted in the the checkSideThreads call next loop
-                                self.xbee.close()                       # Close the xbee so it can be opened again later
-                                print('Xbee is now Enabled')
-                                if(self.rfdEnabled):
-                                    self.ser.write('Xbee is now Enabled')
-                            except Exception, e:
-                                pass
 
             # RFD and GPS Check
             if (not self.gpsEnabled) or (not self.rfdEnabled):
@@ -1110,14 +826,6 @@ if __name__ == "__main__":
     except:
         loggingGPS = False
         print("Failed to create gpslog.txt")
-
-    try:
-        tempLog = open(folder+"templog.txt","a")
-        tempLog.close()
-        loggingTemp = True
-    except:
-        loggingTemp = False
-        print("Failed to create templog.txt")
 
     mainLoop = main()
     while True:
